a45bc8977efb1ecbf17467235dc03dd645b3fe32
abas <abas>
2024-04-01 18:56:26 -0400

    fix links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../assets/logo.svg";
 import { HEADER_MENU_ITEMS } from "../constatnts";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -10,7 +11,7 @@ const Header = () => {
       <ul>
         {HEADER_MENU_ITEMS.map((item, index) => (
           <li key={index}>
-            <a href={item.url}>{item.title}</a>
+            <NavLink to={item.url}>{item.title}</NavLink>
           </li>
         ))}
       </ul>
