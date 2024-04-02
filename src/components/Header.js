02976d651c6d47c3df747198aeabe0177f580fe8
abas <abas>
2024-04-01 20:39:22 -0400

    added responsive navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,29 @@ import React from "react";
 import logo from "../assets/logo.svg";
 import { HEADER_MENU_ITEMS } from "../constatnts";
 import { NavLink } from "react-router-dom";
+import "./style.css";
 
 const Header = () => {
   return (
     <header>
-      <img src={logo} />
-      <nav>
-      <ul>
-        {HEADER_MENU_ITEMS.map((item, index) => (
-          <li key={index}>
-            <NavLink to={item.url}>{item.title}</NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
+      <nav className="navbar">
+        <div className="navbar-container container">
+          <input type="checkbox" name="" id="" />
+          <div className="hamburger-lines">
+            <span className="line line1"></span>
+            <span className="line line2"></span>
+            <span className="line line3"></span>
+          </div>
+          <ul className="menu-items">
+            {HEADER_MENU_ITEMS.map((item, index) => (
+              <li key={index}>
+                <NavLink to={item.url}>{item.title}</NavLink>
+              </li>
+            ))}
+          </ul>
+          <img className="logo" src={logo} />
+        </div>
+      </nav>
     </header>
   );
 };
