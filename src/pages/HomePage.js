e749987d0c6f07cafc12937c4633a139a914e477
abas <abas>
2024-04-13 22:30:11 -0400

    worked on BookingPage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Chicago from "../components/Chicago";
 import Specials from "../components/Specials";
-import { Center, Grid, GridItem } from "@chakra-ui/react";
+import { Center} from "@chakra-ui/react";
 
 export const HomePage = () => {
   return (
