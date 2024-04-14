0e60d47006d4fb1d0315652afd74e12f8fb2c300
abas <abas>
2024-04-14 14:33:02 -0400

    update BookingPage

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
-import { Box } from "@chakra-ui/react";
+import { Card,CardBody, Button, VStack } from "@chakra-ui/react";
+import "./style.css";
 
 export const BookingPage = () => {
   const [formData, setFormData] = useState();
 
   const handleSubmit = () => {};
 
-  return (
-    <div>
+  return ( <VStack marginTop="10"> 
+    <Card maxW="500"><CardBody>
+     
       <form onSubmit={handleSubmit()}>
-        <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date" />
-        <label htmlFor="res-time">Choose time</label>
-        <input type="date" id="res-date" />
-        <label htmlFor="res-time">Choose time</label>
+        <label htmlFor="res-date">Choose date:</label>
+        <input type="date" id="res-date" /><br/>
+        <label htmlFor="res-time">Choose time:</label>
+        <input type="date" id="res-date" /><br/>
+        <label htmlFor="res-time">Choose time:</label>
         <select id="res-time ">
           <option>17:00</option>
           <option>18:00</option>
@@ -21,16 +23,18 @@ export const BookingPage = () => {
           <option>20:00</option>
           <option>21:00</option>
           <option>22:00</option>
-        </select>
-        <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" />
-        <label htmlFor="occasion">Occasion</label>
+        </select><br/>
+        <label htmlFor="guests">Number of guests:</label>
+        <input type="number" placeholder="1" min="1" max="10" id="guests" /><br/>
+        <label htmlFor="occasion">Occasion:</label>
         <select id="occasion">
           <option>Birthday</option>
           <option>Anniversary</option>
-        </select>
-        <input type="submit" value="Make Your reservation" />
+        </select><br/><br/>
+        <Button colorScheme="yellow">Make Your reservation</Button>
+         
       </form>
-    </div>
+      </CardBody>
+      </Card></VStack>
   );
 };
