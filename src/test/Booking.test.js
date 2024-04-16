47044381a9f13584ed1910cd230617aae1ebee20
abas <abas>
2024-04-16 18:41:29 -0400

    added testing

--- src/test/Booking.test.js
+++ src/test/Booking.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from "@testing-library/react";
-import {BookingPage} from '../pages/BookingPage';
+import {BookingForm} from '../components/BookingForm';
 
-test('Renders the BookingForm heading', () => {
-    render(<BookingPage />);
-    const headingElement = screen.getByText("Book Now");
-    expect(headingElement).toBeInTheDocument();
-})
+// test('Renders the BookingForm heading', () => {
+//     render(<BookingPage />);
+//     const headingElement = screen.getByText("Book Now");
+//     expect(headingElement).toBeInTheDocument();
+// })
+
+test('renders BookingForm component', () => {
+    const mockOnFormSubmit = jest.fn();
+    const mockOnChangeDate = jest.fn();
+    const availableTimes = ["12:00", "13:00", "14:00"];
+  
+    render(
+      <BookingForm
+        onSubmit={mockOnFormSubmit}
+        availableTimes={availableTimes}
+        onChangeDate={mockOnChangeDate}
+      />
+    );
+  
+    const dateLabel = screen.getByText(/Date/i);
+    expect(dateLabel).toBeInTheDocument();
+  });
+  
