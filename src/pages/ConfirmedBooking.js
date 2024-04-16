c337921a3cd29c7324d044280f065fe05615c056
abas <abas>
2024-04-16 15:54:55 -0400

    update confirmed

--- src/pages/ConfirmedBooking.js
+++ src/pages/ConfirmedBooking.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { Center } from "@chakra-ui/react";
 
 export const ConfirmedBooking = () => {
-  return <>Confirmed!</>;
+  return (
+    <Center>
+      <p style={{ padding: 50, fontWeight: 700 }}>Confirmed! </p>
+    </Center>
+  );
 };
